ed20cfab6dcfb965756f0dba7d2854aaee4e4798
HaS1k <mr.hasik666@gmail.com>
2025-01-28 18:29:38 +1000

    back_menu

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
 import asyncio
+import requests
+
 
 from aiogram import Bot, Dispatcher,  types
 from aiogram.client.bot import DefaultBotProperties
@@ -20,6 +22,63 @@ load_dotenv(find_dotenv())
 bot = Bot(token=os.getenv('TOKEN'))
 dp =Dispatcher()
 
+# Конфигурация бота и API
+
+API_URL = "https://api.sbis.ru/retail/nomenclature/list?"
+SBIS_TOKEN = os.getenv('SBIS_TOKEN')
+API_HEADERS = {"X-SBISAccessToken": os.getenv("SBIS_TOKEN")}
+API_PARAMS = {
+    "pointId": 7245,
+    "priceListId": 37,
+    "withBalance": True,
+    "withBarcode": True,
+    "onlyPublished": False,
+    "page": 0,
+    "pageSize": 50
+}
+
+# Переменная для хранения данных
+menu_data = {}
+
+# Функция запроса данных из СБИС
+async def fetch_menu():
+    global menu_data
+    response = requests.get(API_URL, params=API_PARAMS, headers=API_HEADERS)
+    if response.status_code == 200:
+        data = response.json().get("nomenclatures", [])
+        
+        categories = {}
+        items = {}
+        
+        for item in data:
+            hierarchical_id = item.get("hierarchicalId")
+            parent_id = item.get("hierarchicalParent")
+            if item.get("id") is None:  # Это категория
+                categories[hierarchical_id] = {
+                    "name": item.get("name"),
+                    "subcategories": [],
+                    "items": []
+                }
+            else:  # Это товар
+                items[hierarchical_id] = {
+                    "name": item.get("name"),
+                    "price": item.get("cost"),
+                    "description": item.get("description"),
+                    "image": f"https://api.sbis.ru/retail{item.get('images', [''])[0]}",
+                    "parent": parent_id
+                }
+        
+        # Связываем товары и категории
+        for item_id, item in items.items():
+            if item["parent"] in categories:
+                categories[item["parent"]]["items"].append(item)
+        
+        menu_data = categories
+        print("Данные обновлены")
+    else:
+        print("Ошибка при запросе к API СБИС")
+
+
 
 
 
@@ -35,8 +94,12 @@ async def start_cmd(message: types.Message):
  ]
  )
  await message.answer(text="start",reply_markup=markap)
+ await message.answer("Загрузка меню...")
+ await fetch_menu()
+ await message.answer("Меню обновлено! Перейдите в веб-приложение для просмотра.")
 
-
+async def get_menu():
+    return menu_data
 
 
 async def  main():
